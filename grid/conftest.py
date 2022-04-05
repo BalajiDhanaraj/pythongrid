63a87779220a644b8b0d525f7b246c7e903d5b81
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-05 11:39:28 +0530

    update

--- grid/conftest.py
+++ grid/conftest.py
@@ -1,9 +1,9 @@
 import allure
 from allure_commons.types import AttachmentType
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
 import pytest
-from webdriver_manager.firefox import GeckoDriverManager
+
 
 
 @pytest.hookimpl(hookwrapper=True, tryfirst=True)
@@ -16,16 +16,16 @@ def pytest_runtest_makereport(item, call):
 
 @pytest.fixture(params=["chrome","firefox"],scope="function")
 def get_browser(request):
-    remote_url = "http://localhost:4444/wd/hub"
+    remote_url = "http://192.168.1.6:4444"
     if request.param == "chrome":
        # driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
        driver = webdriver.Remote(command_executor=remote_url, desired_capabilities={"browserName": "chrome"})
 
-    if request.param == "firefox":
-       # driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-       driver = webdriver.Remote(command_executor=remote_url, desired_capabilities={"browserName": "firefox"})
+    # if request.param == "firefox":
+    #    # driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+    #    driver = webdriver.Remote(command_executor=remote_url, desired_capabilities={"browserName": "firefox"})
 
     driver.get("http://facebook.com")
-    driver.maximize_window()
+
     yield driver
     driver.quit()
--- grid/test_webdriver_integration.py
+++ grid/test_webdriver_integration.py
@@ -1,7 +1,7 @@
 import allure
 from allure_commons.types import AttachmentType
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
 import pytest
 
 
