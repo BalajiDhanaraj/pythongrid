a6e6fa5050ae774f0f21e25841c228c01ef4252c
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-05 11:32:06 +0530

    update

--- testcase/BrowserTest.py
+++ testcase/BrowserTest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 # driver = webdriver.Chrome(executable_path="/Volumes/Macintosh HD/For Mac/python project/Browserdrivers/chromedriver")
 from selenium.webdriver import DesiredCapabilities
 
-remote_url = "http://localhost:6666/wd/hub"
+remote_url = "http://192.168.1.6:4444"
 
 driver = webdriver.Remote(command_executor=remote_url,desired_capabilities={"browserName":"chrome"})
 
