b68bddafca6518d2df03c86653b6f0385e47690b
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-06 10:52:29 +0530

    update

--- ReadingConfig/config_reader.py
+++ ReadingConfig/config_reader.py
@@ -2,8 +2,18 @@ from configparser import ConfigParser
 
 ## reading the config file
 
-config = ConfigParser()
-config.read("config.ini")
+# config = ConfigParser()
+# config.read("config.ini")
+#
+# print(config.get("locator","username"))
+# print(config.get("basic info","testsiteurl"))
 
-print(config.get("locator","username"))
-print(config.get("basic info","testsiteurl"))
+
+
+def readConfig(section,key):
+    config = ConfigParser()
+    config.read("config.ini")
+    return config.get(section,key)
+
+
+print(readConfig("locator","username"))
