6f31491ebbe96c3508871562eb2b38b792901cfc
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-06 10:40:50 +0530

    update

--- Logs/generatinglogs.py
+++ Logs/generatinglogs.py
@@ -1,7 +1,25 @@
 import logging
+## Generating logs
+# logging.basicConfig(filename="/Volumes/Macintosh HD/For Mac/python project/pythongrid/Logs/logfile.log",format='%(asctime)s: %(levelname)s: %(message)s',
+#                     filemode='w',level=logging.INFO)
+# log = logging.getLogger()
+#
+# log.info("this is our first log")
 
-logging.basicConfig(filename="/Volumes/Macintosh HD/For Mac/python project/pythongrid/Logs/logfile.log",format='%(asctime)s: %(levelname)s: %(message)s',
-                    filemode='w',level=logging.INFO)
-log = logging.getLogger()
 
-log.info("this is our first log")
+def log():
+    logging.basicConfig(filename="/Volumes/Macintosh HD/For Mac/python project/pythongrid/Logs/logfile.log",
+                        format='%(asctime)s: %(levelname)s: %(message)s',
+                        filemode='w', level=logging.INFO)
+
+
+    log = logging.getLogger()
+    return log
+
+
+logging =  log()
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
