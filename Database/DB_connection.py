92b0c7cbf96e5d25fd0b6fc23104d45e3bf0d9a2
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-08 13:31:41 +0530

    update

---
+++ Database/DB_connection.py
@@ -0,0 +1,34 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+
+    host="localhost",
+    user="root",
+    password="root@2363",
+    database='pydb'
+
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
