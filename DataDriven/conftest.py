ba5ec6a13880224c36d5000aa88af0dd7b8c9754
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-12 09:25:35 +0530

    update

---
+++ DataDriven/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+
+import allure
+from allure_commons.types import AttachmentType
+from selenium import webdriver
+
+
+
+
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
