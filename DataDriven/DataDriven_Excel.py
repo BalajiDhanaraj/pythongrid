2f8c4b0530ddcc590d5281326d961acf54561b2d
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-12 09:27:51 +0530

    update

--- DataDriven/DataDriven_Excel.py
+++ DataDriven/DataDriven_Excel.py
@@ -1,11 +1,8 @@
 import allure
+import pytest
 from allure_commons.types import AttachmentType
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium import webdriver as driver
-import pytest
+
 
 # parallel test
 
