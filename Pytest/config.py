c0d02574c51bc9a5ed66224625464490cd613c24
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-18 11:05:28 +0530

    update

--- Pytest/config.py
+++ Pytest/config.py
@@ -2,11 +2,11 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture(params=['chrome'],scope='class')
+@pytest.fixture(scope='class')
 def init_driver(request):
     print("-------------------setup----------------------")
     driver = webdriver.Chrome(
-        executable_path="/Volumes/Macintosh HD/For Mac/python project/pythongrid/grid/chromedriver")
+        executable_path="/Volumes/Macintosh HD/For Mac/python project/pythongrid/Pytest/chromedriver")
     request.cls.driver = driver
     yield
     print("----------------------quit----------------------")
