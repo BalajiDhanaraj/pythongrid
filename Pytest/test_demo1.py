6555cc8e02684b3626c2e19c6b3cfdc4dfb2e940
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-19 15:20:50 +0530

    update

--- Pytest/test_demo1.py
+++ Pytest/test_demo1.py
@@ -1,14 +1,14 @@
 # import pytest
 #
-# """  IN Terminal pytest (filename- like test_markers) -m(mean markers) markers_name(login-ex) -v(verbose - its give even more info about testcase """
-#
-# def test_m1():
-#     a = 10
-#     b = 12
-#     assert a == b
-#
-# def test_m2():
-#     assert False
-#
-# def test_login_gmail():
-#     assert "admin" == "admin"
+"""  IN Terminal pytest (filename- like test_markers) -m(mean markers) markers_name(login-ex) -v(verbose - its give even more info about testcase """
+
+def test_m1():
+    a = 10
+    b = 12
+    assert a == b
+
+def test_m2():
+    assert False
+
+def test_login_gmail():
+    assert "admin" == "admin"
