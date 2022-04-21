da998ab826087cdb53661b89948c4e0e4959558d
Balaji.D <46712265+BalajiDhanaraj@users.noreply.github.com>
2022-04-21 10:41:24 +0530

    update

--- Pytest_Fix/config.py
+++ Pytest_Fix/config.py
@@ -1,13 +1,22 @@
 import pytest
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+# @pytest.fixture(params=["chrome","firefox"],scope="function")
+# def get_browser(request):
+#
+#     if request.param == "chrome":
+#         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+#     request.cls.driver = driver
+#     driver.maximize_window()
+#     driver.implicitly_wait(10)
+#     yield driver
+#     driver.quit()
 
 
-@pytest.fixture(scope='class')
-def init_driver(request):
-    print("-------------------setup----------------------")
-    driver = webdriver.Chrome(
-        executable_path="/Volumes/Macintosh HD/For Mac/python project/pythongrid/Pytest/chromedriver")
-    request.cls.driver = driver
-    yield
-    print("----------------------quit----------------------")
-    driver.close()
+@pytest.fixture
+def input_value():
+   input = 39
+   return input
+
+
--- Pytest_Fix/test_fixture.py
+++ Pytest_Fix/test_fixture.py
@@ -6,12 +6,20 @@ from selenium import webdriver
 
 
 
-@pytest.mark.usefixtures("init_driver")
-class Base:
-    pass
+# @pytest.mark.usefixtures("get_browser")
+# class BaseTest:
+#     pass
+#
+# class Test_google(BaseTest):
+#
+#     def test_chrome(self):
+#         self.driver.get("http://www.google.com")
+#         assert self.driver.title == "Google"
 
-class Test_google(Base):
 
-    def test_chrome(self):
-        self.driver.get("http://www.google.com")
-        assert self.driver.title == "Google"
+
+def test_divisible_by_3(input_value):
+   assert input_value % 3 == 0
+
+def test_divisible_by_6(input_value):
+   assert input_value % 6 == 0
